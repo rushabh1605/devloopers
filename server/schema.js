5fec0334d8f3082ea9a62d903ab7ff0e6cbcb6ee
deepali04 <nagwade.deepali@gmail.com>
2023-04-24 19:00:41 -0400

    Added schema and resolver for top scorer

--- server/schema.js
+++ server/schema.js
@@ -23,9 +23,62 @@ const typeDefs = gql`
     goalsConceded: Int
   }
 
+  type FixtureByDate {
+    id: Int!
+    venueName: String
+    venueCity: String
+    matchDate: Date
+    matchTime: String
+    matchTimeZone: String
+    matchStatus: String
+    league: String
+    country: String
+    leagueLogo: String
+    season: Int
+    homeTeamName: String
+    homeTeamID: String
+    homeTeamLogo: String
+    awayTeamName: String
+    awayTeamID: String
+    awayTeamLogo: String
+    homeTeamGoals: Int
+    awayTeamGoals: Int
+    homeHalfTimeScore: Int
+    homeFullTimeScore: Int
+    awayHalfTimeScore: Int
+    awayFullTimeScore: Int
+  }
+
+  type LeagueTopScorer {
+    playerID: Int!
+    firstName: String
+    lastName: String
+    age: Int
+    Nationality: String
+    playerImage: String
+    playerHeight: String
+    playerWeight: String
+    playerPosition: String
+    isInjured: Boolean
+    teamName: String
+    teamLogo: String
+    appearances: Int
+    lineUps: Int
+    season: Int
+    goals: Int
+    assists: Int
+    penaltyScored: Int
+    penaltyMissed: Int
+    
+  }
+
+  scalar Date
+
   type Query {
     LeagueInformation: [League]
     StandingInformation(league: Int!, season: Int!): [TableStandings]
+    FixtureByDateInformation(matchDate: Date!) : [FixtureByDate]
+    TopScorerByLeague(league: Int!, season: Int!): [LeagueTopScorer]
   }
 `;
 
