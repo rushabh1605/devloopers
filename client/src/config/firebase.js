d16b02ee63a09f45a45cf33170704e9a816246c0
Rushabh Thakkar <62187076+rushabh1605@users.noreply.github.com>
2023-04-27 18:57:12 -0400

    Update firebase.js

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,8 +1,19 @@
 
-import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from "firebase/auth"
-const firebaseConfig = {
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth"
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA4nTKwHkFrJLsi4R2OzBC3m4HCJ76OX7I",
+//     authDomain: "supastrikers-3c9d2.firebaseapp.com",
+//     projectId: "supastrikers-3c9d2",
+//     storageBucket: "supastrikers-3c9d2.appspot.com",
+//     messagingSenderId: "1043522331224",
+//     appId: "1:1043522331224:web:fcea762f31eba1002b602d",
+//     measurementId: "G-F8Z7C3RJEQ"
+// };
+const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyA4nTKwHkFrJLsi4R2OzBC3m4HCJ76OX7I",
     authDomain: "supastrikers-3c9d2.firebaseapp.com",
     projectId: "supastrikers-3c9d2",
@@ -10,10 +21,13 @@ const firebaseConfig = {
     messagingSenderId: "1043522331224",
     appId: "1:1043522331224:web:fcea762f31eba1002b602d",
     measurementId: "G-F8Z7C3RJEQ"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+export default firebaseApp;
+
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// //const analytics = getAnalytics(app);
+// export const auth = getAuth(app);
+// export const googleProvider = new GoogleAuthProvider();
