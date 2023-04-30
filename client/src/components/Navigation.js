d966c4442652bf65d5cccc27031367f4a69acf0c
Rushabh Thakkar <62187076+rushabh1605@users.noreply.github.com>
2023-04-29 22:11:13 -0400

    Update Navigation.js

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,17 +1,21 @@
-import React, {useContext} from 'react';
-import {NavLink} from 'react-router-dom';
-import {AuthContext} from '../firebase/Auth';
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../firebase/Auth';
 import SignOutButton from './SignOut';
 import '../App.css';
-
+import logo from './img/logo.png';
 const Navigation = () => {
-  const {currentUser} = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
   return <div>{currentUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>;
 };
 
 const NavigationAuth = () => {
   return (
     <nav className='navigation'>
+      <img src={logo} className='App-logo' alt='logo' />
+      <h1 className='App-title'>
+        Welcome to the SupaStrika
+      </h1>
       <ul>
         <li>
           <NavLink to='/'>Landing</NavLink>
@@ -21,6 +25,12 @@ const NavigationAuth = () => {
         </li>
         <li>
           <NavLink to='/account'>Account</NavLink>
+        </li>
+        <li>
+          <NavLink to='/about'>About</NavLink>
+        </li>
+        <li>
+          <NavLink to='/news'>News</NavLink>
         </li>
         <li>
           <SignOutButton />
