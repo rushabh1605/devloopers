a3a3a3320637cb8f52a54f4f8e5eef4b8be128db
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2023-05-04 12:24:24 -0400

    updated changes in navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {NavLink , BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Account from './Account';
 import Home from './Home';
 import Landing from './Landing';
@@ -12,17 +12,78 @@ import PrivateRoute from './PrivateRoute';
 import About from './About';
 import News from './News';
 import NotFound from './NotFound';
+import logo from '../logo.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider
+} from '@apollo/client';
+
+
+
+
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  })
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <AuthProvider>
-      <Router>
-        <div className='App'>
-          <header className='App-header'>
-            <Navigation />
-          </header>
-        </div>
-        <Routes>
-          <Route path='/' element={<Landing />} />
+    <Router>
+    <div className='App'>
+      <header className='App-header'>
+        
+    <Navbar collapseOnSelect expand="lg" className='bg' >
+    <Container>
+      
+    <Navbar.Brand ><span class="navbar-brand">SUPASTRIKERS</span></Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto" >
+                <NavLink className='nav-links' to='/'>
+                  Home
+                </NavLink>
+
+                <NavLink className='nav-links' to='/about'>
+                  About
+                </NavLink>
+
+                <NavLink className='nav-links' to='/leagues'>
+                  Leagues
+                </NavLink>
+                
+                <NavLink className='nav-links' to='/account'>
+                  Account
+                </NavLink>
+
+                <NavLink className='nav-links' to='/signin'>
+                  LogIn
+                </NavLink>
+
+                <NavLink className='nav-links' to='/signup'>
+                  SignUp
+                </NavLink>
+
+
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+      </header>
+      
+      <div className='App-body'>
+      <Routes>
+        <Route path='/' element={<Landing />} />
           <Route path='/home' element={<PrivateRoute />}>
             <Route path='/home' element={<Home />} />
           </Route>
@@ -34,9 +95,13 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/news' element={<News />} />
           <Route path="/404" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+      </Routes>
+      </div>
+      
+    </div>
+  </Router>
+  </AuthProvider>
+    </ApolloProvider>
   );
 }
 
