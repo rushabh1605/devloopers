64b2cba8732709328ebeda4dbadcc95bc8e0ba15
Rushabh Thakkar <62187076+rushabh1605@users.noreply.github.com>
2023-05-04 22:39:50 -0400

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import {NavLink , BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { NavLink, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Account from './Account';
 import Home from './Home';
 import Landing from './Landing';
@@ -12,7 +12,7 @@ import PrivateRoute from './PrivateRoute';
 import About from './About';
 import News from './News';
 import NotFound from './NotFound';
-import logo from '../logo.jpg';
+//import logo from '../logo.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -38,69 +38,69 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <AuthProvider>
-    <Router>
-    <div className='App'>
-      <header className='App-header'>
-        
-    <Navbar collapseOnSelect expand="lg" className='bg' >
-    <Container>
-      
-    <Navbar.Brand ><span class="navbar-brand">SUPASTRIKERS</span></Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto" >
-                <NavLink className='nav-links' to='/'>
-                  Home
-                </NavLink>
+      <AuthProvider>
+        <Router>
+          <div className='App'>
+            <header className='App-header'>
 
-                <NavLink className='nav-links' to='/about'>
-                  About
-                </NavLink>
+              <Navbar collapseOnSelect expand="lg" className='bg' >
+                <Container>
 
-                <NavLink className='nav-links' to='/leagues'>
-                  Leagues
-                </NavLink>
-                
-                <NavLink className='nav-links' to='/account'>
-                  Account
-                </NavLink>
+                  <Navbar.Brand ><span class="navbar-brand">SUPASTRIKERS</span></Navbar.Brand>
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                  <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto" >
+                      <NavLink className='nav-links' to='/'>
+                        Home
+                      </NavLink>
 
-                <NavLink className='nav-links' to='/signin'>
-                  LogIn
-                </NavLink>
+                      <NavLink className='nav-links' to='/about'>
+                        About
+                      </NavLink>
 
-                <NavLink className='nav-links' to='/signup'>
-                  SignUp
-                </NavLink>
+                      <NavLink className='nav-links' to='/leagues'>
+                        Leagues
+                      </NavLink>
+
+                      <NavLink className='nav-links' to='/account'>
+                        Account
+                      </NavLink>
+
+                      <NavLink className='nav-links' to='/signin'>
+                        LogIn
+                      </NavLink>
+
+                      <NavLink className='nav-links' to='/signup'>
+                        SignUp
+                      </NavLink>
 
 
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-      </header>
-      
-      <div className='App-body'>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-          <Route path='/home' element={<PrivateRoute />}>
-            <Route path='/home' element={<Home />} />
-          </Route>
-          <Route path='/account' element={<PrivateRoute />}>
-            <Route path='/account' element={<Account />} />
-          </Route>
-          <Route path='/signin' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/news' element={<News />} />
-          <Route path="/404" element={<NotFound />} />
-      </Routes>
-      </div>
-      
-    </div>
-  </Router>
-  </AuthProvider>
+                    </Nav>
+                  </Navbar.Collapse>
+                </Container>
+              </Navbar>
+            </header>
+
+            <div className='App-body'>
+              <Routes>
+                <Route path='/' element={<Landing />} />
+                <Route path='/home' element={<PrivateRoute />}>
+                  <Route path='/home' element={<Home />} />
+                </Route>
+                <Route path='/account' element={<PrivateRoute />}>
+                  <Route path='/account' element={<Account />} />
+                </Route>
+                <Route path='/signin' element={<SignIn />} />
+                <Route path='/signup' element={<SignUp />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/news' element={<News />} />
+                <Route path="/404" element={<NotFound />} />
+              </Routes>
+            </div>
+
+          </div>
+        </Router>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
