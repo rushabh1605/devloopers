c335855386a3d50320f2e72d7849336fea768601
Rushabh Thakkar <62187076+rushabh1605@users.noreply.github.com>
2023-05-06 19:23:42 -0400

    Update SignUp.js

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,98 +1,137 @@
-import React, {useContext, useState} from 'react';
-import {Navigate} from 'react-router-dom';
-import {doCreateUserWithEmailAndPassword} from '../firebase/FirebaseFunctions';
-import {AuthContext} from '../firebase/Auth';
-import SocialSignIn from './SocialSignIn';
-function SignUp() {
-  const {currentUser} = useContext(AuthContext);
-  const [pwMatch, setPwMatch] = useState('');
-  const handleSignUp = async (e) => {
-    e.preventDefault();
-    const {displayName, email, passwordOne, passwordTwo} = e.target.elements;
-    if (passwordOne.value !== passwordTwo.value) {
-      setPwMatch('Passwords do not match');
-      return false;
+import React, { useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import queries from "../queries";
+import { useMutation } from "@apollo/client";
+import Swal from "sweetalert2";
+import { isLoggedIn } from "../helper";
+
+const Signup = () => {
+  let [signUp] = useMutation(queries.CREATE);
+  let navigate = useNavigate();
+  useEffect(() => {
+    if (isLoggedIn()) {
+      navigate("/", { replace: true });
     }
-
-    try {
-      await doCreateUserWithEmailAndPassword(
-        email.value,
-        passwordOne.value,
-        displayName
-      );
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  if (currentUser) {
-    return <Navigate to='/home' />;
-  }
-
+  }, []);
+  let userName;
+  let password;
   return (
-    <div>
-      <h1>Sign up</h1>
-      {pwMatch && <h4 className='error'>{pwMatch}</h4>}
-      <form onSubmit={handleSignUp}>
-        <div className='form-group'>
-          <label>
-            Name:
-            <input
-              className='form-control'
-              required
-              name='displayName'
-              type='text'
-              placeholder='Name'
-            />
-          </label>
+    <div className="main">
+      <section className="signup">
+        <div className="container1">
+          <div className="signup-content">
+            <form
+              className="signup-form"
+              onSubmit={async (e) => {
+                e.preventDefault();
+                try {
+                  if (
+                    !userName ||
+                    !userName.value ||
+                    userName.value.trim() == ""
+                  ) {
+                    Swal.fire({
+                      title: "Error!",
+                      text: "Please enter username to signup!",
+                      icon: "error",
+                      confirmButtonText: "I'll fix it!",
+                    });
+                    return;
+                  }
+                  if (!password || !password.value || password.value == "") {
+                    Swal.fire({
+                      title: "Error!",
+                      text: "Please enter password to signup!",
+                      icon: "error",
+                      confirmButtonText: "I'll fix it!",
+                    });
+                    return;
+                  }
+
+                  const thisUser = await signUp({
+                    variables: {
+                      userName: userName.value,
+                      password: password.value,
+
+                    },
+                  });
+
+                  if (thisUser.data.createUser) {
+                    Swal.fire({
+                      title: "Awesome!",
+                      text: `${thisUser.data.createUser.name}, your account has been created. Click the button to login.`,
+                      icon: "success",
+                      confirmButtonText: "Lets go!",
+                    }).then((isConfirmed) => {
+                      if (isConfirmed.value === true) navigate("/");
+                    });
+                  }
+                } catch (e) {
+                  if (e.message.slice(e.message.length - 3) == "401") {
+                    Swal.fire({
+                      title: "Oops!",
+                      text: `We could not create your account?`,
+                      icon: "error",
+                      confirmButtonText: "I'll fix it!",
+                    });
+                  } else {
+                    Swal.fire({
+                      title: "Oops!",
+                      text: `${e.message}`,
+                      icon: "error",
+                      confirmButtonText: "I'll fix it!",
+                    });
+                  }
+                }
+              }}
+            >
+              <h2 className="form-title">Create account</h2>
+              <br />
+
+              <div className="form-group ">
+                <label>
+                  Enter your Username
+                  <br />
+                  <input
+                    className="form-input"
+                    ref={(node) => (userName = node)}
+                  ></input>
+                </label>
+              </div>
+
+              <div className="form-group ">
+                <label>
+                  Enter your password
+                  <br />
+                  <input
+                    className="form-input"
+                    type="password"
+                    ref={(node) => (password = node)}
+                  ></input>
+                </label>
+              </div>
+
+              <br />
+
+              <button className="btn-lg btn-danger" type="submit">
+                Sign Up
+              </button>
+            </form>
+            <p className="loginhere">
+              Have already an account ?{" "}
+              <Link
+                to={`/signin`}
+                className="loginhere-link"
+                variant="contained"
+              >
+                Log in
+              </Link>
+            </p>
+          </div>
         </div>
-        <div className='form-group'>
-          <label>
-            Email:
-            <input
-              className='form-control'
-              required
-              name='email'
-              type='email'
-              placeholder='Email'
-            />
-          </label>
-        </div>
-        <div className='form-group'>
-          <label>
-            Password:
-            <input
-              className='form-control'
-              id='passwordOne'
-              name='passwordOne'
-              type='password'
-              placeholder='Password'
-              autoComplete='off'
-              required
-            />
-          </label>
-        </div>
-        <div className='form-group'>
-          <label>
-            Confirm Password:
-            <input
-              className='form-control'
-              name='passwordTwo'
-              type='password'
-              placeholder='Confirm Password'
-              autoComplete='off'
-              required
-            />
-          </label>
-        </div>
-        <button id='submitButton' name='submitButton' type='submit'>
-          Sign Up
-        </button>
-      </form>
-      <br />
-      <SocialSignIn />
+      </section>
     </div>
   );
-}
+};
 
-export default SignUp;
+export default Signup;
