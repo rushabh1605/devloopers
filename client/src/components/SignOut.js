083a58946dd82d1a4856822747f0aef69c5a0199
Rushabh Thakkar <62187076+rushabh1605@users.noreply.github.com>
2023-05-06 19:25:48 -0400

    Update SignOut.js

--- client/src/components/SignOut.js
+++ client/src/components/SignOut.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import {doSignOut} from '../firebase/FirebaseFunctions';
+import { useState } from 'react';
 
 const SignOutButton = () => {
+
+  const [authenticated, setAuthenticated] = useState(true);
+
+  const handleLogout = () => {
+    // Clear authentication token or session from local storage or cookies
+    localStorage.removeItem('authToken');
+
+    // Update authenticated state
+    setAuthenticated(false);
+
+    // Redirect to login page or public page
+    window.location.href = '/';
+  };
+
   return (
-    <button type='button' onClick={doSignOut}>
-      Sign Out
-    </button>
+    <button onClick={handleLogout}>Logout</button>
   );
 };
 
