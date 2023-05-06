e19d07b84ac75fc2c1789ecbd39079498a5ffce7
Rushabh Thakkar <62187076+rushabh1605@users.noreply.github.com>
2023-05-06 19:25:43 -0400

    Update SignIn.js

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,81 +1,137 @@
-import React, {useContext} from 'react';
-import SocialSignIn from './SocialSignIn';
-import {Navigate} from 'react-router-dom';
-import {AuthContext} from '../firebase/Auth';
-import {
-  doSignInWithEmailAndPassword,
-  doPasswordReset,
-} from '../firebase/FirebaseFunctions';
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import queries from "../queries";
+import { useSelector, useDispatch } from "react-redux";
+import actions from "../actions";
+import { useLazyQuery, useMutation, useQuery } from "@apollo/client";
+import Swal from "sweetalert2";
+import { isLoggedIn } from "../helper";
 
-function SignIn() {
-  const {currentUser} = useContext(AuthContext);
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    let {email, password} = event.target.elements;
+const Login = () => {
+  let navigate = useNavigate();
+  useEffect(() => {
+    if (isLoggedIn()) {
+      navigate("/main", { replace: true });
+    }
+  }, []);
+  let dispatch = useDispatch();
+  const [loginUser] = useLazyQuery(
+    queries.LOGIN,
+    {
+      enabled: false,
+    },
+    {
+      fetchPolicy: "cache-and-network",
+    }
+  );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function PostData(e) {
+    e.preventDefault();
 
     try {
-      await doSignInWithEmailAndPassword(email.value, password.value);
+      if (!email || email.trim() == "") {
+        Swal.fire({
+          title: "Error!",
+          text: "Please enter email to login!",
+          icon: "error",
+          confirmButtonText: "I'll fix it!",
+        });
+        setEmail("");
+        return;
+      }
+      if (!password || password == "") {
+        Swal.fire({
+          title: "Error!",
+          text: "Please enter password to login!",
+          icon: "error",
+          confirmButtonText: "I'll fix it!",
+        });
+        setPassword("");
+        return;
+      }
+      const { data } = await loginUser({ variables: { email, password } });
+      if (data.loginUser === null) {
+        throw new Error("Either username or password is invalid");
+      }
+      if (data.loginUser && data.loginUser !== null) {
+        dispatch(actions.storeToken(data.loginUser.token));
+        localStorage.setItem("token", data.loginUser.token);
+        localStorage.setItem("userId", data.loginUser.userId);
+        Swal.fire({
+          title: "Yay!",
+          text: "Successful Login",
+          icon: "success",
+        });
+        setEmail("");
+        setPassword("");
+        navigate("/main");
+      }
     } catch (error) {
-      alert(error);
+      console.log(error);
+      Swal.fire({
+        title: "Oops!",
+        text: error.message,
+        icon: "error",
+        confirmButtonText: "I'll fix it!",
+      });
     }
-  };
-
-  const passwordReset = (event) => {
-    event.preventDefault();
-    let email = document.getElementById('email').value;
-    if (email) {
-      doPasswordReset(email);
-      alert('Password reset email was sent');
-    } else {
-      alert(
-        'Please enter an email address below before you click the forgot password link'
-      );
-    }
-  };
-  if (currentUser) {
-    return <Navigate to='/home' />;
   }
+
   return (
-    <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <div className='form-group'>
-          <label>
-            Email:
-            <input
-              className='form-control'
-              name='email'
-              id='email'
-              type='email'
-              placeholder='Email'
-              required
-            />
-          </label>
-        </div>
-        <div className='form-group'>
-          <label>
-            Password:
-            <input
-              className='form-control'
-              name='password'
-              type='password'
-              placeholder='Password'
-              autoComplete='off'
-              required
-            />
-          </label>
-        </div>
-        <button type='submit'>Log in</button>
+    <div className="main1">
+      <section className="signup">
+        <div className="container1">
+          <div className="signup-content">
+            <form className="signup-form" onSubmit={PostData}>
+              <h2 className="form-title">Login</h2>
 
-        <button className='forgotPassword' onClick={passwordReset}>
-          Forgot Password
-        </button>
-      </form>
+              <div className="form-group">
+                <label>
+                  Enter your email
+                  <br />
+                  <input
+                    className="form-input"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                  ></input>
+                </label>
+              </div>
 
-      <br />
-      <SocialSignIn />
+              <div className="form-group">
+                <label>
+                  Enter your password
+                  <br />
+                  <input
+                    className="form-input"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                  ></input>
+                </label>
+              </div>
+              <br />
+
+              <button className="btn-lg btn-danger" type="submit">
+                Log In
+              </button>
+            </form>
+            <p className="loginhere">
+              Don't have an account ?
+              <Link
+                to={`/signup`}
+                className="loginhere-link"
+                variant="contained"
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
-export default SignIn;
+export default Login;
